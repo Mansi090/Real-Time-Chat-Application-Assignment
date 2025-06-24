914c1bd418b28bf27150e418fbdca9cd0fbbbe3d
Mansi090 <mansid875@gmail.com>
2025-06-24 11:39:25 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://real-time-chat-application-assignme.vercel.app"
+    ],
     methods: ["GET", "POST"]
   }
 });
